4bc82f91999784f6c369a67adc5fcffb6327165f
Susan Jarvis <susjar@ceh.ac.uk>
2022-10-19 16:26:23 +0100

    Collation of WCBS data

--- WCBS/Calculate_Responses_WCBS.R
+++ WCBS/Calculate_Responses_WCBS.R
@@ -13,24 +13,38 @@ shandiv <- function(x){
 
 
 # Get data
-fpath <- config_path
+dir <- config::get()
+fpath <- dir$directories$ukbmsdata
 
-wcbs_ukbms <- read.csv(paste0(fpath, "Citizen science datasets/UKBMS_WCBS/site_data_2017-19.txt"))
+wcbs_ukbms <- read.table(paste0(fpath, "site_date_2017-2021.txt"), sep = "\t", header = TRUE)
 
-# Subset to UKBMS
+# Subset to WCBS
 wcbs_sites <- wcbs_ukbms[wcbs_ukbms$SCHEME == "WCBS",]
 
-# Get location data
-CSlocs <- read_excel(paste0(fpath, "Citizen science datasets/CitizenScienceGridRefs.xlsx"), col_types = c("text", "text", "text", "numeric", "numeric"))
-wcbs_locs <- filter(CSlocs, SCHEME == "WCBS")
-
 # Get species data
-wcbs_ukbms_species <- read.csv(paste0(fpath,"Citizen science datasets/UKBMS_WCBS/count _data_2017-2019.txt"))
+wcbs_ukbms_species <- read.table(paste0(fpath,"count_data_2017-2021_v2.txt"), sep = ",", header = TRUE)
 
 # filter to UKBMS
 wcbs_species <- filter(wcbs_ukbms_species, SCHEME == "WCBS" & SITENO %in% wcbs_sites$SITENO)
 
-# get transect length and add to site data
+
+
+#species - site match
+
+unique(wcbs_sites$SITENO[!wcbs_sites$SITENO %in% wcbs_species$SITENO])
+#one site with no species data
+
+
+# Get location data
+scpath <- dir$directories$scoredata
+CSlocs <- read.csv(paste0(scpath, "Butts_Gradient_Scores.csv"))
+CSlocs <- CSlocs[!duplicated(CSlocs[,2:14]),2:14]#remove duplicate rows, some squares have more than one site number due to multiple transects in that location
+#remove locations for masked squares
+CSlocs <- CSlocs[CSlocs$MaskStatus == "OK",]#also includes squares with NA mask which are coastal
+wcbs_locs <- CSlocs[CSlocs$buttsurv.SITENO %in% wcbs_sites$SITENO,]
+
+
+
 # check if each site has always the same transect length
 wcbs_species %>% group_by(SITENO) %>% 
   summarise(n = length(unique(TRANSECT_LENGTH))) %>% 
@@ -41,15 +55,27 @@ wcbs_transect <- select(wcbs_species, SITENO, TRANSECT_LENGTH) %>%
 janitor::get_dupes(wcbs_transect, SITENO)
 # no duplicates
 
+
 # all metadata
 wcbs_sites <- full_join(wcbs_sites, wcbs_transect) %>%
   select(SITENO, EAST, NORTH, YEAR, N_VISITS_MAYTOAUGUST, TRANSECT_LENGTH) %>%
-  full_join(select(wcbs_locs, SITENO = `SITE CODE`,
-                   GRIDREF_1KM) %>%
+  full_join(select(wcbs_locs, SITENO = `buttsurv.SITENO`,
+                   buttsurv.GRIDREF_1km) %>%
               mutate(SITENO = as.numeric(SITENO)))
 summary(wcbs_sites)
 
 # Deal with taxonomy issues ####
+
+# Deal with taxonomy issues ####
+unique(grep("Fritillary",wcbs_species$COMMON_NAME, value = TRUE, ignore.case = TRUE))
+# no aggregate here
+
+unique(grep("white",wcbs_species$COMMON_NAME, value = TRUE, ignore.case = TRUE))
+# no aggregate here
+
+unique(grep("skipper",wcbs_species$COMMON_NAME, value = TRUE, ignore.case = TRUE))
+# aggregate for Essex and Small Skipper
+
 wcbs_species %>% filter(COMMON_NAME == "Painted Lady") %>%
   .$COUNT %>% summary()
 
@@ -62,10 +88,22 @@ wcbs_responses <- wcbs_species %>%
   summarise(Abundance = sum(COUNT),
             Richness = sum(COUNT>0),
             Shannon_diversity = shandiv(COUNT)) %>%
-  inner_join(wcbs_sites)
+  right_join(wcbs_sites)
+
+#replace one missing transect length
+wcbs_responses$TRANSECT_LENGTH <- 2000
+
+#replace NA values in Richness, Abundance and Shannon_diversity with 0
+wcbs_responses[,c("Abundance", "Richness", "Shannon_diversity")][is.na(wcbs_responses[,c("Abundance", "Richness", "Shannon_diversity")])] <- 0
+
+#remove masked grid references
+
+wcbs_responses <- wcbs_responses[!is.na(wcbs_responses$buttsurv.GRIDREF_1km),]
+
+wcbs_responses <- wcbs_responses[!duplicated(wcbs_responses),]
 
 summary(wcbs_responses)
 psych::multi.hist(select_if(wcbs_responses, is.numeric))
 
 plot(Abundance ~ N_VISITS_MAYTOAUGUST, wcbs_responses)
-plot(Abundance ~ N_VISITS_MAYTOAUGUST, wcbs_responses)
+plot(Richness ~ N_VISITS_MAYTOAUGUST, wcbs_responses)
