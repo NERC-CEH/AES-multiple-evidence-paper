94932013fbeed2bc435b7c857724da909547e95c
Fiona Seaton <fseaton@ceh.ac.uk>
2022-11-16 11:29:13 +0000

    Add PCA scores and remove temp AR models

--- WCBS/AES_Models_WCBS.R
+++ WCBS/AES_Models_WCBS.R
@@ -1,8 +1,6 @@
 #' WCBS - Analyse each response to AES scores plus environmental gradients
 #' 
-# library(MASS)
 library(DHARMa)
-# library(car)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
@@ -44,7 +42,7 @@ wcbs_aes <- AES %>%
 
 #' Combine all data and scale
 WCBS_all_data <- wcbs_responses %>%
-  # inner_join(PCA, by = c("buttsurv.GRIDREF_1km" = "PLAN_NO","YEAR")) %>%
+  inner_join(PCA, by = c("buttsurv.GRIDREF_1km" = "PLAN_NO","YEAR")) %>%
   inner_join(wcbs_aes, by = c("buttsurv.GRIDREF_1km" = "CELLCODE","YEAR")) %>%
   ungroup() %>% 
   filter(AES1KM < 50000) %>%
@@ -69,205 +67,100 @@ summary(WCBS_all_data)
 mod_pr <- prior(normal(0,1), class = b) +
   prior(student_t(3,0,1), class = sd)
 Rich_wcbs_mod <- brm(Richness ~ AES1KM*AES3KM + 
-                        N_VISITS_MAYTOAUGUST + 
-                        (1|SITENO),
-                      data = WCBS_all_data, family = "poisson", prior = mod_pr,
-                      cores = 4)
+                       N_VISITS_MAYTOAUGUST + YR +
+                       Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
+                       (1|SITENO),
+                     data = WCBS_all_data, family = "poisson", prior = mod_pr,
+                     cores = 4)
 summary(Rich_wcbs_mod)
 plot(Rich_wcbs_mod)
 pp_check(Rich_wcbs_mod)
-# not too far away, but still underpredicting at low counts and overpredicting at
-# medium counts
+# not too far away, but still slightly underpredicting at low counts and
+# overpredicting at medium counts
+plot(conditional_effects(Rich_wcbs_mod, effects = "AES1KM:AES3KM",
+                         int_conditions = list(AES3KM = c(0.1,0.25,0.75))),
+     rug = TRUE, theme = ggplot2::theme_classic())
 
-# Temp AR
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Rich_wcbs_mod_temp <- brm(Richness ~ AES1KM*AES3KM + 
-                             N_VISITS_MAYTOAUGUST + 
-                             ar(time = YRnm, gr = SITENO),
-                           data = WCBS_all_data, family = "poisson", prior = mod_pr,
-                           cores = 4)
-summary(Rich_wcbs_mod_temp)
-plot(Rich_wcbs_mod_temp)
-pp_check(Rich_wcbs_mod_temp)
-# not too far away, but still underpredicting at low counts and overpredicting at
-# medium counts
 
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(student_t(3,0,1), class = sd) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Rich_wcbs_mod_temp2 <- brm(Richness ~ AES1KM*AES3KM + 
-                              N_VISITS_MAYTOAUGUST + 
-                              ar(time = YRnm, gr = SITENO) + (1|SITENO),
-                            data = WCBS_all_data, family = "poisson", prior = mod_pr,
-                            cores = 4)
-summary(Rich_wcbs_mod_temp2)
-plot(Rich_wcbs_mod_temp2)
-pp_check(Rich_wcbs_mod_temp2)
-# If include the SITE random effect then the model cannot resolve the temporal
-# autocorrelation - have to pick one or the other. No improvement to fit to data
 
 # Check model residuals with DHARMa
 model.check <- createDHARMa(
-  simulatedResponse = t(posterior_predict(Rich_wcbs_mod_temp)),
+  simulatedResponse = t(posterior_predict(Rich_wcbs_mod)),
   observedResponse = WCBS_all_data$Richness,
-  fittedPredictedResponse = apply(t(posterior_epred(Rich_wcbs_mod_temp)), 1, mean),
+  fittedPredictedResponse = apply(t(posterior_epred(Rich_wcbs_mod)), 1, mean),
   integerResponse = TRUE)
 
 plot(model.check) # highly problematic for KS and dispersion
 
-# Negative binomial models
 mod_pr <- prior(normal(0,1), class = b) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar) +
-  prior(gamma(0.01,0.01), class = shape)
-Rich_wcbs_mod_temp3 <- brm(Richness ~ AES1KM*AES3KM + 
-                              N_VISITS_MAYTOAUGUST + 
-                              ar(time = YRnm, gr = SITENO),
-                            data = WCBS_all_data, family = "negbinomial", 
-                            prior = mod_pr,
-                            cores = 4)
-summary(Rich_wcbs_mod_temp3)
-plot(Rich_wcbs_mod_temp3)
-pp_check(Rich_wcbs_mod_temp3)
-
-model.check <- createDHARMa(
-  simulatedResponse = t(posterior_predict(Rich_wcbs_mod_temp3)),
-  observedResponse = WCBS_all_data$Richness,
-  fittedPredictedResponse = apply(t(posterior_epred(Rich_wcbs_mod_temp3)), 1, mean),
-  integerResponse = TRUE)
-
-plot(model.check)
-# still bad - KS, Dispersion and outlier test all failed
-
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(student_t(3,0,1), class = sd) +
-  prior(gamma(0.01,0.01), class = shape)
-Rich_wcbs_mod_temp4 <- brm(Richness ~ AES1KM*AES3KM + 
-                              N_VISITS_MAYTOAUGUST + 
-                              (1|SITENO),
-                            data = WCBS_all_data, family = "negbinomial", 
-                            prior = mod_pr,
-                            cores = 4)
-summary(Rich_wcbs_mod_temp4)
-plot(Rich_wcbs_mod_temp4)
-pp_check(Rich_wcbs_mod_temp4)
-
-model.check <- createDHARMa(
-  simulatedResponse = t(posterior_predict(Rich_wcbs_mod_temp4)),
-  observedResponse = WCBS_all_data$Richness,
-  fittedPredictedResponse = apply(t(posterior_epred(Rich_wcbs_mod_temp4)), 1, mean),
-  integerResponse = TRUE)
-
-plot(model.check)
-# Even worse!
-
-# Revisit this when the PCA axes are included - there's a problem with not predicting
-# the low numbers in some squares, which might be related to environmental conditions
-
+  prior(student_t(3,0,1), class = sd)
+Rich_wcbs_mod2 <- brm(Richness ~ AES1KM*AES3KM + 
+                       N_VISITS_MAYTOAUGUST + YR +
+                       Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
+                       (1|SITENO),
+                     data = WCBS_all_data, family = "negbinomial", prior = mod_pr,
+                     cores = 4)
+summary(Rich_wcbs_mod2)
+plot(Rich_wcbs_mod2)
+pp_check(Rich_wcbs_mod2)
+# no improvement in model fit to data, shape parameter is estimated to be 634 +/- 184
 
 #' ### Abundance models
 # poisson models 
 
 mod_pr <- prior(normal(0,1), class = b) +
   prior(student_t(3,0,1), class = sd)
-Abun_wcbs_mod <- brm(Abundance ~ AES1KM*AES3KM + 
-                        N_VISITS_MAYTOAUGUST + 
-                        (1|SITENO),
-                      data = WCBS_all_data, family = "poisson", prior = mod_pr,
-                      cores = 4)
+Abun_wcbs_mod <- brm(Abundance ~ AES1KM*AES3KM +
+                        N_VISITS_MAYTOAUGUST + YR + 
+                       Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
+                       (1|SITENO),
+                     data = WCBS_all_data, family = "poisson", prior = mod_pr,
+                     cores = 4)
 # does not converge - Rhat of intercept and group-level effects too high
 # negative binomial - with site RE
-Abun_wcbs_mod <- brm(Abundance ~ AES1KM*AES3KM + 
-                        N_VISITS_MAYTOAUGUST + 
-                        (1|SITENO),
-                      data = WCBS_all_data, family = "negbinomial", prior = mod_pr,
-                      cores = 4)
+Abun_wcbs_mod <- brm(Abundance ~ AES1KM*AES3KM +
+                       N_VISITS_MAYTOAUGUST + YR + 
+                       Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
+                       (1|SITENO),
+                     data = WCBS_all_data, family = "negbinomial", prior = mod_pr,
+                     cores = 4)
 summary(Abun_wcbs_mod)
 plot(Abun_wcbs_mod)
 pp_check(Abun_wcbs_mod)
 pp_check(Abun_wcbs_mod) +
   scale_x_continuous(limits = c(0,1000))
-# reasonable recovery of data - worth revisiting when env covariates included
 pp_check(Abun_wcbs_mod, type = "ecdf_overlay") + 
   scale_x_continuous(limits = c(0,1000))
-
-# negative binomial with temp AR
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Abun_wcbs_mod_temp <- brm(Abundance ~ AES1KM*AES3KM + 
-                             N_VISITS_MAYTOAUGUST + 
-                             ar(time = YRnm, gr = SITENO),
-                           data = WCBS_all_data, family = "negbinomial", 
-                           prior = mod_pr,
-                           cores = 4)
-# Rhat = 1.09 - shape parameter, ar[1] and sderr also unconverged. If fit with
-# poisson then still doesn't converge, and all parameters bad, not just the shape
-# parameter
-summary(Abun_wcbs_mod_temp)
-
-# Negative binomial with site RE and temp AR
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(student_t(3,0,1), class = sd) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Abun_wcbs_mod_temp2 <- brm(Abundance ~ AES1KM*AES3KM + 
-                              N_VISITS_MAYTOAUGUST + 
-                              ar(time = YRnm, gr = SITENO) + (1|SITENO),
-                            data = WCBS_all_data, family = "negbinomial", 
-                            prior = mod_pr,
-                            cores = 4)
-# 4 divergent transitions and autoregressive part of model is not being identified
-summary(Abun_wcbs_mod_temp2)
-plot(Abun_wcbs_mod_temp2)
-pp_check(Abun_wcbs_mod_temp2)
-# check divergent transitions
-pairs(Abun_wcbs_mod_temp2$fit, 
-      pars = c("sd_SITENO__Intercept","shape","ar[1]",
-               "sderr","b_Intercept"), 
-      log = TRUE)
-# all divergent transitions at high sderr and ar[1] 0.0-0.6
-
-# May be possible to get rid of them through modifying adapt_delta but model is
-# performing so badly at resolving the autocorrelation that I think it may not be
-# worth trying that.
+plot(conditional_effects(Abun_wcbs_mod, effects = "AES1KM:AES3KM",
+                         int_conditions = list(AES3KM = c(0.1,0.25,0.75))),
+     rug = TRUE, theme = ggplot2::theme_classic())
 
 
 #' ### Diversity models
 mod_pr <- prior(normal(0,1), class = b) +
   prior(student_t(3,0,1), class = sd)
 Div_wcbs_mod <- brm(Shannon_diversity ~ AES1KM*AES3KM + 
-                       N_VISITS_MAYTOAUGUST + 
+                       N_VISITS_MAYTOAUGUST + YR +
+                      Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
                        (1|SITENO),
                      data = WCBS_all_data, prior = mod_pr,
                      cores = 4)
 summary(Div_wcbs_mod)
 plot(Div_wcbs_mod)
 pp_check(Div_wcbs_mod)
-# slight overprediction at low diversity and underprediction at high
+# poorly fitted
 
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Div_wcbs_mod_temp <- brm(Shannon_diversity ~ AES1KM*AES3KM + 
-                            N_VISITS_MAYTOAUGUST + 
-                            ar(time = YRnm, gr = SITENO),
-                          data = WCBS_all_data, prior = mod_pr,
-                          cores = 4)
-summary(Div_wcbs_mod_temp)
-plot(Div_wcbs_mod_temp)
-pp_check(Div_wcbs_mod_temp)
-
-
-mod_pr <- prior(normal(0,1), class = b) +
-  prior(student_t(3,0,1), class = sd) +
-  prior(uniform(-1,1), lb = -1, ub = 1, class = ar)
-Div_wcbs_mod_temp2 <- brm(Shannon_diversity ~ AES1KM*AES3KM + 
-                             N_VISITS_MAYTOAUGUST + 
-                             ar(time = YRnm, gr = SITENO) + (1|SITENO),
-                           data = WCBS_all_data, prior = mod_pr,
-                           cores = 4)
-# ESS warning
-summary(Div_wcbs_mod_temp2)
-plot(Div_wcbs_mod_temp2)
-pp_check(Div_wcbs_mod_temp2)
-# ar[1] resolved to be 0, and sd_SITENO is the same as the random effect on site
-# number without the temporal autocorrelation. All 3 models resolve the data to the
-# same level of accuracy
+WCBS_all_data$expDiversity <- exp(WCBS_all_data$Shannon_diversity)
+Div_wcbs_mod <- brm(expDiversity ~ AES1KM*AES3KM + 
+                      N_VISITS_MAYTOAUGUST + YR +
+                      Climate_PC1 + Landscape_PC1 + Habitat_PC1 +
+                      (1|SITENO),
+                    data = WCBS_all_data, prior = mod_pr,
+                    cores = 4)
+summary(Div_wcbs_mod)
+plot(Div_wcbs_mod)
+pp_check(Div_wcbs_mod)
+# good fit to data
+plot(conditional_effects(Div_wcbs_mod, effects = "AES1KM:AES3KM",
+                         int_conditions = list(AES3KM = c(0.1,0.25,0.75))),
+     rug = TRUE, theme = ggplot2::theme_classic())
