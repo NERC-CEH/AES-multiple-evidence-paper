a51b878c31fb5b109b59413bbe2936dd8acaa739
Susan Jarvis <susjar@ceh.ac.uk>
2022-11-30 10:01:21 +0000

    Add combined plot

--- Integrated_models/Combined_plots.R
+++ Integrated_models/Combined_plots.R
@@ -12,7 +12,7 @@ library(ggplot2)
 theme_set(theme_classic())
 library(brms)
 library(ggeffects)
-
+library(patchwork)
 
 # folder setup for saving
 dir <- config::get()
@@ -29,12 +29,9 @@ range(Rich_LS_mod$data$AES1KM)
 range(Rich_WCBS_mod$data$AES1KM)
 range(Rich_UKBMS_mod$data$AES1KM)
 
-range(Rich_LS_mod$data$AES3KM)
-range(Rich_WCBS_mod$data$AES3KM)
-range(Rich_UKBMS_mod$data$AES3KM)
 
 
-mean(c(Rich_LS_mod$data$AES1KM, Rich_WCBS_mod$data$AES1KM, Rich_UKBMS_mod$data$AES1KM))
+
 mean(c(Rich_LS_mod$data$AES3KM, Rich_WCBS_mod$data$AES3KM, Rich_UKBMS_mod$data$AES3KM))
 
 mean(c(Rich_LS_mod$data$Climate_PC1, Rich_WCBS_mod$data$Climate_PC1, Rich_UKBMS_mod$data$Climate_PC1))
@@ -94,4 +91,72 @@ rich_1km <- ggplot(p4, aes(x = x, y = predicted, colour = group, fill = group)) 
   NULL
 rich_1km
 
+ggsave(paste0(modpath,"Combined plots/Butterfly richness 1km individual schemes.png"), height = 800, width = 1000, units = "mm", scale = 0.15)
+
+##3km plots
+
+
+range(Rich_LS_mod$data$AES3KM)
+range(Rich_WCBS_mod$data$AES3KM)
+range(Rich_UKBMS_mod$data$AES3KM)
+
+mean(c(Rich_LS_mod$data$AES1KM, Rich_WCBS_mod$data$AES1KM, Rich_UKBMS_mod$data$AES1KM))
+
+
+p1 <- ggpredict(Rich_LS_mod, "AES1KM[-0.60:2.42, by = 0.5]",
+                condition = c("AES3KM" = "0.362",
+                              "ROUND_NUMBER" = "0.4",
+                              "SURVEY_YEAR" = "2018",
+                              "Climate_PC1" = "-0.04",
+                              "Landscape_PC1" = "-0.05",
+                              "Habitat_PC1" = "0.26"))
+
+p2 <- ggpredict(Rich_WCBS_mod, "AES1KM[-0.60:6.66, by = 0.5]",
+                condition = c("AES3KM" = "0.362",
+                              "N_VISITS_MAYTOAUGUST" = "0.4",
+                              "SURVEY_YEAR" = "2018",
+                              "Climate_PC1" = "-0.04",
+                              "Landscape_PC1" = "-0.05",
+                              "Habitat_PC1" = "0.26"))
+
+p3 <- ggpredict(Rich_UKBMS_mod, "AES1KM[-0.60:7.38, by = 0.5]",
+                condition = c("AES3KM" = "0.362",
+                              "N_VISITS_MAYTOAUGUST" = "0.4",
+                              "SURVEY_YEAR" = "2018",
+                              "Climate_PC1" = "-0.04",
+                              "Landscape_PC1" = "-0.05",
+                              "Habitat_PC1" = "0.26"))
+
+p1$group <- "LandSpAES"
+p2$group <- "UKBMS"
+p3$group <- "WCBS"
+
+
+p4 <- do.call(rbind, list(p2, p1, p3)) %>%
+  mutate(x = (x*5000 + 3000)/1000)
+rich_3km <- ggplot(p4, aes(x = x, y = predicted, colour = group, fill = group)) + 
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), 
+              alpha = 0.3, colour = NA) +
+  geom_line() +
+  # geom_rug(data = p1_raw, aes(x = x, y = response, colour = group),
+  #          sides = "b") +
+  # geom_rug(data = p2_raw, aes(x = x, y = response, colour = group),
+  #          sides = "t") +
+  # geom_rug(data = p3_raw, aes(x = x, y = response, colour = group),
+  #          sides = "t", outside = TRUE) +
+  coord_cartesian(clip = "off") +
+  scale_fill_manual(aesthetics = c("fill","colour"),
+                    values = my_col,
+                    name = "Survey") +
+  scale_y_continuous(limits = c(0,30), expand = c(0,0)) +
+  # scale_x_continuous(limits = c(0,74000)) +
+  labs(x = "AES 3km ('000s)", y = "Predicted Butterfly Richness") +
+  NULL
+rich_3km
+
+ggsave(paste0(modpath,"Combined plots/Butterfly richness 3km individual schemes.png"), height = 800, width = 1000, units = "mm", scale = 0.15)
+
+rich_1km + rich_3km + plot_layout(guides='collect') &
+  theme(legend.position='bottom')
+ggsave(paste0(modpath,"Combined plots/Butterfly richness combined 1km and 3km individual schemes.png"), height = 800, width = 1200, units = "mm", scale = 0.15)
 
